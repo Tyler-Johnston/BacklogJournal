253354e2775f4505e72bd35ab729e3a1f7880517
Bethany Christensen <a02299515@aggies.usu.edu>
2024-11-12 20:03:43 -0700

    setup basic router nav stuff

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TestPageComponent } from './test-page/test-page.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'test', component:TestPageComponent},
+    {path: 'home', component:HomeComponent}
+];
