53ad2cb1b08f7881d26e156a726287dc6e7d83cb
Bethany Christensen <bethiehc@gmail.com>
2024-11-22 19:54:08 -0700

    got the start of the materials angular set up

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [MatCardModule, MatToolbarModule, MatSlideToggleModule, CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
